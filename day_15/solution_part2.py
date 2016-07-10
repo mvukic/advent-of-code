f7ad434e3db2b09715c3de933642ce5e1cc66dd5
Matija-Vukic <matija.vukic@hotmail.com>
2016-07-10 21:03:32 +0200

    part 1 working

--- day_15/solution_part2.py
+++ day_15/solution_part2.py
@@ -6,6 +6,7 @@ def Main():
 	ingredients={}
 	combinations = []
 	result={}
+	calories={}
 	with open('input.txt',mode='r') as data:
 		for line in data:
 			line=line.rstrip()
@@ -18,18 +19,30 @@ def Main():
 				"texture":nums[3],
 				"calories":nums[4]
 			}
-	for comb in itertools.combinations(range(1,101),len(ingredients)):
+	for comb in list(itertools.combinations(range(0,101),4)):
 		if sum(comb) == 100:
 			for perm in itertools.permutations(comb):
 				combinations.append(perm)
+	print(len(combinations))
 	for comb in combinations:
 		(a,b,c,d) = (ingredients["Sprinkles"],ingredients["PeanutButter"],ingredients["Frosting"],ingredients["Sugar"])
 		cap= comb[0]*a["capacity"]  +comb[1]*b["capacity"]  +comb[2]*c["capacity"]  +comb[3]*d["capacity"]
 		dur= comb[0]*a["durability"]+comb[1]*b["durability"]+comb[2]*c["durability"]+comb[3]*d["durability"]
 		fla= comb[0]*a["flavor"]    +comb[1]*b["flavor"]    +comb[2]*c["flavor"]    +comb[3]*d["flavor"]
 		tex= comb[0]*a["texture"]   +comb[1]*b["texture"]   +comb[2]*c["texture"]   +comb[3]*d["texture"]
-		if not (cap < 0 or dur < 0 or fla<0 or tex<0):
-			result[comb] = cap*dur*fla*tex
+		cal= comb[0]*a["calories"]  +comb[1]*b["calories"]  +comb[2]*c["calories"]  +comb[3]*d["calories"] 
+		if cap < 0:
+			cap=0
+		if dur < 0:
+			dur=0
+		if fla < 0:
+			fla=0  
+		if tex < 0:
+			tex=0
+		r = cap*dur*fla*tex
+		if r != 0 and cal == 500:
+			result[comb] = r
+			# print("{} score: {} cal: {}".format(comb,r,cal))  
 	sort = sorted(result,key=result.get,reverse=True)
 	print("{} gives best score of {}".format(sort[0],result[sort[0]]))
 
---
+++ day_15/test.py
@@ -0,0 +1,39 @@
+import re
+import itertools
+import functools
+import collections
+import operator
+
+text = open('input.txt').read().strip().split('\n')
+
+total = 100
+things = {}
+for line in text:
+    ing, stuff = line.split(':')
+    things[ing] = {k:int(v) for prop in stuff.split(',') for k, v in [prop.strip().split(' ')]}
+
+def tally(combo):
+    total = collections.Counter()
+    for ing, n in combo.items():
+        for k, v in things[ing].items():
+            total[k] += n*v
+    for k, v in total.items():
+        if v < 0:
+            total[k] = 0
+    calories = total['calories']
+    total['calories'] = 1
+    # part 2
+    # if calories != 500: total['calories'] = 0
+    return functools.reduce(operator.mul, total.values())
+
+def partition(N, d):
+    if d==1: yield [N]
+    else: yield from ( [H]+T for H in range(N+1) for T in partition(N-H, d-1) )
+
+best = 0
+for combo in (dict(zip(things, combo)) for combo in partition(100, 4)):
+    total = tally(combo)
+    print(combo)
+    if total > best:
+        best = total
+print(best)
